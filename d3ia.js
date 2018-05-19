4dfa8037fc2f8f72fdee725c5a24ed7026f6279f
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-05-19 17:28:56 +0100

    Added then function to handle Promise and make example code work.

--- d3ia.js
+++ d3ia.js
@@ -1,20 +1,24 @@
-// import d3 from './d3.js'
+// Page 68
+// The cities.csv data drawn as a bar chart using the maximum value of population attribute in the domain setting of the scale
 
-// (() => { 
-    d3.csv("cities.csv", (data, i, array, error) => { 
-        if(error) console.log(error);
-        dataViz(data) 
-    })
-// })();
-
-function dataViz(incomingData) {
-    console.log(incomingData.label)
-
-    d3.select("body")
-    .selectAll("div.cities")
-    .data(incomingData)
-    .enter()
-    .append("div")
-    .attr("class", "cities")
-    .html(d => d.label);
-}
+d3.csv("cities.csv")
+ .then(function dataViz(incomingData) { 
+    var maxPopulation = d3.max(incomingData, d => parseInt(d.population)); 
+    var yScale = d3.scaleLinear().domain([0, maxPopulation]).range([0, 460]); 
+    
+    d3.select("svg")
+     .attr("style", "height: 480px; width: 600px;"); 
+     
+    d3.select("svg")
+     .selectAll("rect")
+     .data(incomingData)
+     .enter()
+     .append("rect")
+     .attr("width", 50)
+     .attr("height", d => yScale(parseInt(d.population)))
+     .attr("x", (d, i) => i * 60)
+     .attr("y", d => 480 - yScale(parseInt(d.population)))
+     .style("fill", "#FE9922")
+     .style("stroke", "#9A8B7A")
+     .style("stroke-width", "1px") 
+})
