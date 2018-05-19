2cb877a6f11149acbcedc90cba530a2308e97418
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-05-19 21:21:01 +0100

    P72 Tweets as circles.

--- d3ia.js
+++ d3ia.js
@@ -30,7 +30,7 @@
 
 d3.json("tweets.json")
  .then( (data) => { dataViz(data.tweets) });
-
+// Bar Chart
 function dataViz(incomingData) {
     var nestedTweets = d3.nest()
         .key(d => d.user)
@@ -53,3 +53,29 @@ function dataViz(incomingData) {
         .style("stroke", "#9A8B7A")
         .style("stroke-width", "1px");
 }
+
+// Circles
+function dataViz(incomingData) {
+    incomingData.forEach(d => {
+        d.impact = d.favorites.length + d.retweets.length;
+        d.tweetTime = new Date(d.timestamp);
+    })
+    var maxImpact = d3.max(incomingData, d => d.impact);
+    var startEnd = d3.extent(incomingData, d => d.tweetTime);
+    var timeRamp = d3.scaleTime().domain(startEnd).range([20,480]);
+    var yScale = d3.scaleLinear().domain([0,maxImpact]).range([0,460]);
+    var radiusScale = d3.scaleLinear().domain([0,maxImpact]).range([1,20]);
+    var colourScale = d3.scaleLinear().domain([0,maxImpact]).range(["white", "#75739F"]);
+
+    d3.select("svg")
+     .selectAll("circle")
+     .data(incomingData)
+     .enter()
+     .append("circle")
+     .attr("r", d => radiusScale(d.impact))
+     .attr("cx", d => timeRamp(d.tweetTime))
+     .attr("cy", d => 480 - yScale(d.impact))
+     .style("fill", d => colourScale(d.impact))
+     .style("stroke", "black")
+     .style("stroke-width", "1px");
+};
