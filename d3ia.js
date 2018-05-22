124cf3f7119372079f19c5d8dd38f878800a7a41
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-05-22 16:17:08 +0100

    Using exit to remove elements from p77.

--- d3ia.js
+++ d3ia.js
@@ -30,54 +30,28 @@
 
 d3.json("tweets.json")
  .then( (data) => { dataViz(data.tweets) });
-// Bar Chart
-function dataViz(incomingData) {
-    var nestedTweets = d3.nest()
-        .key(d => d.user)
-        .entries(incomingData);
-    nestedTweets.forEach(d => {
-        d.numTweets = d.values.length;
-    });
-    var maxTweets = d3.max(nestedTweets, d => d.numTweets);
-    var yScale = d3.scaleLinear().domain([0, maxTweets]).range([0,500]);
-    d3.select("svg")
-        .selectAll("rect")
-        .data(nestedTweets)
-        .enter()
-        .append("rect")
-        .attr("width", 50)
-        .attr("height", d => yScale(d.numTweets))
-        .attr("x", (d,i) => i * 60)
-        .attr("y", d => 500 - yScale(d.numTweets))
-        .style("fill", "#FE9922")
-        .style("stroke", "#9A8B7A")
-        .style("stroke-width", "1px");
-}
-
-// Circles
+// // Bar Chart
 // function dataViz(incomingData) {
-//     incomingData.forEach(d => {
-//         d.impact = d.favorites.length + d.retweets.length;
-//         d.tweetTime = new Date(d.timestamp);
-//     })
-//     var maxImpact = d3.max(incomingData, d => d.impact);
-//     var startEnd = d3.extent(incomingData, d => d.tweetTime);
-//     var timeRamp = d3.scaleTime().domain(startEnd).range([20,480]);
-//     var yScale = d3.scaleLinear().domain([0,maxImpact]).range([0,460]);
-//     var radiusScale = d3.scaleLinear().domain([0,maxImpact]).range([1,20]);
-//     var colourScale = d3.scaleLinear().domain([0,maxImpact]).range(["white", "#75739F"]);
-
+//     var nestedTweets = d3.nest()
+//         .key(d => d.user)
+//         .entries(incomingData);
+//     nestedTweets.forEach(d => {
+//         d.numTweets = d.values.length;
+//     });
+//     var maxTweets = d3.max(nestedTweets, d => d.numTweets);
+//     var yScale = d3.scaleLinear().domain([0, maxTweets]).range([0,500]);
 //     d3.select("svg")
-//      .selectAll("circle")
-//      .data(incomingData)
-//      .enter()
-//      .append("circle")
-//      .attr("r", d => radiusScale(d.impact))
-//      .attr("cx", d => timeRamp(d.tweetTime))
-//      .attr("cy", d => 480 - yScale(d.impact))
-//      .style("fill", d => colourScale(d.impact))
-//      .style("stroke", "black")
-//      .style("stroke-width", "1px");
+//         .selectAll("rect")
+//         .data(nestedTweets)
+//         .enter()
+//         .append("rect")
+//         .attr("width", 50)
+//         .attr("height", d => yScale(d.numTweets))
+//         .attr("x", (d,i) => i * 60)
+//         .attr("y", d => 500 - yScale(d.numTweets))
+//         .style("fill", "#FE9922")
+//         .style("stroke", "#9A8B7A")
+//         .style("stroke-width", "1px");
 // }
 
 // Circles
@@ -93,18 +67,50 @@ function dataViz(incomingData) {
     var radiusScale = d3.scaleLinear().domain([0,maxImpact]).range([1,20]);
     var colourScale = d3.scaleLinear().domain([0,maxImpact]).range(["white", "#75739F"]);
 
-    var tweetG = d3.select("svg")
-     .selectAll("g")
-     .data(incomingData)
+    d3.select("svg")
+     .selectAll("circle")
+     .data(incomingData, JSON.stringify)
      .enter()
-     .append("g")
-     .attr("transform", d => "translate(" + timeRamp(d.tweetTime) + "," + (480 - yScale(d.impact)) + ")");
-    
-    tweetG.append("circle")
+     .append("circle")
      .attr("r", d => radiusScale(d.impact))
+     .attr("cx", d => timeRamp(d.tweetTime))
+     .attr("cy", d => 480 - yScale(d.impact))
      .style("fill", d => colourScale(d.impact))
      .style("stroke", "black")
      .style("stroke-width", "1px");
-    tweetG.append("text")
-     .text(d => d.user + "-" + d.tweetTime.getHours());
+
+     var filteredData = incomingData.filter(d => d.impact > 0)
+     d3.selectAll("circle")
+      .data(filteredData, d => JSON.stringify(d))
+      .exit()
+      .remove();
 }
+
+// Circles
+// function dataViz(incomingData) {
+//     incomingData.forEach(d => {
+//         d.impact = d.favorites.length + d.retweets.length;
+//         d.tweetTime = new Date(d.timestamp);
+//     })
+//     var maxImpact = d3.max(incomingData, d => d.impact);
+//     var startEnd = d3.extent(incomingData, d => d.tweetTime);
+//     var timeRamp = d3.scaleTime().domain(startEnd).range([20,480]);
+//     var yScale = d3.scaleLinear().domain([0,maxImpact]).range([0,460]);
+//     var radiusScale = d3.scaleLinear().domain([0,maxImpact]).range([1,20]);
+//     var colourScale = d3.scaleLinear().domain([0,maxImpact]).range(["white", "#75739F"]);
+
+//     var tweetG = d3.select("svg")
+//      .selectAll("g")
+//      .data(incomingData)
+//      .enter()
+//      .append("g")
+//      .attr("transform", d => "translate(" + timeRamp(d.tweetTime) + "," + (480 - yScale(d.impact)) + ")");
+    
+//     tweetG.append("circle")
+//      .attr("r", d => radiusScale(d.impact))
+//      .style("fill", d => colourScale(d.impact))
+//      .style("stroke", "black")
+//      .style("stroke-width", "1px");
+//     tweetG.append("text")
+//      .text(d => d.user + "-" + d.tweetTime.getHours());
+// }
