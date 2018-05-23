d77a5f2dd1d589a9f654ebfa8603cd1cdc0d06bd
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-05-23 15:48:04 +0100

    Added animated transitions and mouse event element highlighting.

--- soccerviz.js
+++ soccerviz.js
@@ -15,12 +15,39 @@ function createSoccerViz() {
       .attr("transform", (d, i) => "translate(" + (i * 50) + ", 0)")
     var teamG = d3.selectAll("g.overallG");
     teamG
-      .append("circle")
+      .append("circle").attr("r", 0)
+      .transition()
+      .delay((d,i) => i * 100)
+      .duration(500)
+      .attr("r", 40)
+      .transition()
+      .duration(500)
       .attr("r", 20)
     teamG
       .append("text")
       .attr("y", 30)
       .text(d => d.team)
+    teamG
+      .select("text").style("pointer-events", "none");
+
+      // Highlight teams from same region
+    teamG.on("mouseover", highlightRegion);
+    function highlightRegion(d,i) {
+      d3.select(this).select("text").classed("active", true).attr("y", 10)
+        d3.selectAll("g.overallG").select("circle")
+          .each(function (p) {
+            p.region === d.region ?
+              d3.select(this).classed("active", true) :
+              d3.select(this).classed("inactive", true) 
+        });
+      this.parentElement.appendChild(this);
+    }
+    teamG.on("mouseout", unHighlight)
+    function unHighlight() {
+      d3.selectAll("g.overallG").select("circle").attr("class", "")
+      d3.selectAll("g.overallG").select("text")
+      .classed("active", false).attr("y", 30)
+    }
 
     // Buttons
     const dataKeys = Object.keys(incomingData[0])
@@ -35,7 +62,7 @@ function createSoccerViz() {
       var maxValue = d3.max(incomingData, d => parseFloat(d[datapoint]))
       var radiusScale = d3.scaleLinear()
         .domain([0, maxValue]).range([2, 20])
-      d3.selectAll("g.overallG").select("circle")
+      d3.selectAll("g.overallG").select("circle").transition().duration(1000)
         .attr("r", d => radiusScale(d[datapoint]))
     }
   }
