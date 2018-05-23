8c78422a24072ac24ab94f5da56a0aa41b15b165
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-05-23 07:24:31 +0100

    Added buttons and events.

--- soccerviz.js
+++ soccerviz.js
@@ -1,25 +1,42 @@
 function createSoccerViz() {
-    d3.csv("worldcup.csv")
-      .then(data => { overallTeamViz(data) })
+  d3.csv("worldcup.csv")
+    .then(data => { overallTeamViz(data) })
 
-    function overallTeamViz(incomingData) {
-        d3.select("svg")
-          .append("g")
-          .attr("id", "teamsG")
-          .attr("transform", "translate(50,300)")
-          .selectAll("g")
-          .data(incomingData)
-          .enter()
-          .append("g")
-          .attr("class", "overallG")
-          .attr("transform", (d,i) => "translate(" + (i * 50) + ", 0)")
-        var teamG = d3.selectAll("g.overallG");
-        teamG
-          .append("circle")
-          .attr("r", 20)
-        teamG
-          .append("text")
-          .attr("y", 30)
-          .text(d => d.team)    
+  function overallTeamViz(incomingData) {
+    d3.select("svg")
+      .append("g")
+      .attr("id", "teamsG")
+      .attr("transform", "translate(50,300)")
+      .selectAll("g")
+      .data(incomingData)
+      .enter()
+      .append("g")
+      .attr("class", "overallG")
+      .attr("transform", (d, i) => "translate(" + (i * 50) + ", 0)")
+    var teamG = d3.selectAll("g.overallG");
+    teamG
+      .append("circle")
+      .attr("r", 20)
+    teamG
+      .append("text")
+      .attr("y", 30)
+      .text(d => d.team)
+
+    // Buttons
+    const dataKeys = Object.keys(incomingData[0])
+      .filter(d => d !== "team" && d !== "region")
+    d3.select("#controls").selectAll("button.teams")
+      .data(dataKeys).enter()
+      .append("button")
+      .on("click", buttonClick)
+      .html(d => d);
+
+    function buttonClick(datapoint) {
+      var maxValue = d3.max(incomingData, d => parseFloat(d[datapoint]))
+      var radiusScale = d3.scaleLinear()
+        .domain([0, maxValue]).range([2, 20])
+      d3.selectAll("g.overallG").select("circle")
+        .attr("r", d => radiusScale(d[datapoint]))
     }
+  }
 }
